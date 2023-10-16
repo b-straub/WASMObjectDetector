540bdf8dee2a83472718a8449d6705562c995507
Bernhard Straub <bstraubka@hotmail.com>
2023-10-16 09:12:56 +0200

    Add intial files
    Add the intial files for the project

---
+++ WASMObjectDetectorShared/Logic/ImageHelpers.cs
@@ -0,0 +1,38 @@
+﻿using Compunet.YoloV8.Plotting;
+using SixLabors.Fonts;
+
+namespace WASMObjectDetectorShared.Logic;
+
+public class WADetectionPlottingOptions : IDetectionPlottingOptions
+{
+    public float TextHorizontalPadding { get; set; }
+
+    public float BoxBorderThickness { get; set; }
+
+    public ColorPalette ColorPalette { get; set; }
+
+    public FontFamily FontFamily { get; set; }
+
+    public float FontSize { get; set; }
+
+    public WADetectionPlottingOptions()
+    {
+        TextHorizontalPadding = 5F;
+        BoxBorderThickness = 1F;
+        ColorPalette = ColorPalette.Default;
+    }
+
+    public static async Task<WADetectionPlottingOptions> CreateAsync(HttpClient httpClient)
+    {
+        WADetectionPlottingOptions plottingOptions = new();
+
+        var fontBytes = await httpClient.GetByteArrayAsync("_content/WASMObjectDetectorShared/fonts/OpenSans-Regular.ttf");
+        using var fontStream = new MemoryStream(fontBytes);
+        FontCollection collection = new();
+        collection.Add(fontStream);
+
+        plottingOptions.FontFamily = collection.Get("Open Sans");
+        plottingOptions.FontSize = 12F;
+        return plottingOptions;
+    }
+}
---
+++ WASMObjectDetectorShared/Services/ObjectDetectorService.cs
@@ -0,0 +1,54 @@
+﻿using Compunet.YoloV8;
+using Compunet.YoloV8.Plotting;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using WASMObjectDetectorShared.Logic;
+
+namespace WASMObjectDetectorShared.Services
+{
+    public sealed class ObjectDetectorService : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        private YoloV8? _detector;
+        private WADetectionPlottingOptions? _plottingOptions;
+
+        public ObjectDetectorService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task InitializeAsync()
+        {
+            using var detectorStream = await _httpClient.GetStreamAsync("_content/WASMObjectDetectorShared/models/yolov8s.onnx");
+
+            _detector = new(detectorStream);
+            _plottingOptions = await WADetectionPlottingOptions.CreateAsync(_httpClient);
+        }
+
+        public bool IsInitialized()
+        {
+            return _detector is not null && _plottingOptions is not null;
+        }
+
+        public async Task<string> DetectAsync(string imageURL)
+        {
+            ArgumentNullException.ThrowIfNull(_detector, nameof(_detector));
+            ArgumentNullException.ThrowIfNull(_plottingOptions, nameof(_plottingOptions));
+
+            using var imageStream = await _httpClient.GetStreamAsync(imageURL);
+
+            var imageSharp = await Image.LoadAsync(imageStream);
+            var detection = await _detector.DetectAsync(imageSharp);
+
+            Console.WriteLine(detection.Speed.ToString());
+
+            using var ploted = await detection.PlotImageAsync(imageSharp, _plottingOptions);
+            return ploted.ToBase64String(PngFormat.Instance);
+        }
+
+        public void Dispose()
+        {
+            _detector?.Dispose();
+        }
+    }
+}
